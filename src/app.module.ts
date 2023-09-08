d00a6a93433e77c73817bd79dbb15dc0a4931243
ayo6706 <ayo6706@gmail.com>
2023-09-08 09:10:54 +0100

    fix auth implementation

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,25 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { AuthService } from './auth/auth.service';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [MongooseModule.forRoot('mongodb://localhost:27017', {dbName: 'catdb'}),
-    CatsModule],
-  controllers: [AppController],
-  providers: [AppService],
+    CatsModule,
+    AuthModule,
+    UsersModule,
+    JwtModule.register({
+      secret: 'secret',
+      signOptions: { expiresIn: '60s' },
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    })],
+    controllers: [AppController],
+  providers: [AppService, AuthService, JwtService],
 })
 export class AppModule {}
